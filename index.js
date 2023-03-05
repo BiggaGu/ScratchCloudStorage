f2914241e865fd428f820a6e29f02ab414e5b75f
BiggaGu <127008882+BiggaGu@users.noreply.github.com>
2023-03-05 07:54:28 +0000

    Fix Attempt

--- index.js
+++ index.js
@@ -3,14 +3,16 @@ import { ScratchCloud } from "@errorgamer2000/scratch-cloud";
 async function main() {
   const cloud = new ScratchCloud();
 
-  await cloud.login("Thought_Bot", "back2front!");
+  await cloud.login("Thought_Code", "back2front!");
 
   const session = cloud.createSession(
     "813752626",
     false /* do not use TurboWarp servers */
   );
 
-  session.on("set", (name: string, value: string) => {
-    session.set("TEST", 150);
+  session.enableAutoPrefix();
+
+  session.on("set", (name, value) => {
+    session.set("TEST", 500);
   });
 }
