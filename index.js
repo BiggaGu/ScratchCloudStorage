5f228e4197858c3220768cebe40d1e7a6a819b7a
BiggaGu <127008882+BiggaGu@users.noreply.github.com>
2023-03-05 19:20:19 +0000

    attempt fix

--- index.js
+++ index.js
@@ -7,21 +7,18 @@ async function main() {
 
     await cloud.login("Thought_Bot", "back2front!");
 
-    var data = {
-
-    }
+    const data = {};
+    let success = false;
 
     const session = cloud.createSession(
       "813752626",
       false /* do not use TurboWarp servers */
     );
 
-    let success = false;
-
     session.on("set", (name, value) => {
       if(name === "DATA_STATUS" && value === 1){
         if(strToNum.decode(session.get("CURRENT_USER")) in data){
-          data.strToNum.decode(session.get("CURRENT_USER")) = strToNum.encode(session.get("DATA"));
+          data[strToNum.decode(session.get("CURRENT_USER"))] = strToNum.encode(session.get("DATA"));
         } else {
           data[strToNum.encode(session.get("CURRENT_USER"))] = strToNum.encode(session.get("DATA"));
         }
@@ -31,7 +28,7 @@ async function main() {
       }
       if(name === "DATA_STATUS" && value === 2){
         if(strToNum.decode(session.get("CURRENT_USER")) in data){
-          session.set("DATA", strToNum.decode(data.session.get("CURRENT_USER")));
+          session.set("DATA", strToNum.decode(data[strToNum.encode(session.get("CURRENT_USER"))]));
           success = true;
           console.log("Data retrieved. User: " + strToNum.decode(session.get("CURRENT_USER")));
         }
