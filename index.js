9fb5f54a17feaa041380dd13192da69d0e3618da
BiggaGu <127008882+BiggaGu@users.noreply.github.com>
2023-03-05 07:35:54 +0000

    Start Project

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
+import { ScratchCloud } from "@errorgamer2000/scratch-cloud";
 
+async function main() {
+  const cloud = new ScratchCloud();
+
+  await cloud.login("Thought_Bot", "back2front!");
+
+  const session = cloud.createSession(
+    "813752626",
+    false /* do not use TurboWarp servers */
+  );
+
+  session.on("set", (name: string, value: string) => {
+    session.set("TEST", 150);
+  });
+}
